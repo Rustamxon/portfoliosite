34e2b74260a1197aa3a6bc822c0fac77fff12d42
xcoder94 <o7451155@yandex.com>
2022-07-03 00:02:39 +0500

    First commit

---
+++ server.py
@@ -0,0 +1,45 @@
+from lib2to3.pgen2.token import NEWLINE
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def save_to_txt_db(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        db_file = database.write(
+            f'\n {email},{subject},{message} '
+        )
+
+
+def save_to_csv_db(data):
+    with open('database2.csv', newline='',  mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(
+            database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        save_to_csv_db(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong, try again !'
